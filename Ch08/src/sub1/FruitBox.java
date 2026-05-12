ba124a9d362cbfdb7d978983bbb7ba27b199df41
jamie010323 <jamie010323@gmail.com>
2026-05-12 11:47:29 +0900

    add Ch08

---
+++ Ch08/src/sub1/FruitBox.java
@@ -0,0 +1,18 @@
+package sub1;
+
+//치환변수 T 를 갖는
+public class FruitBox<T> {
+	
+	private T fruit;
+	
+	public T getFruit() {
+		return fruit;
+	}
+	
+	public void setFruit (T fruit) {
+		this.fruit = fruit;
+	}
+
+	
+	
+}
